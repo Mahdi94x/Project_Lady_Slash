73f204836f06061ae43429f2319e36ca9561ce6f
Mahdi Mahmoud <mahdim.mahmoud@gmail.com>
2025-07-30 20:28:09 +0300

    Echo Character Animations (cont.)
    - Created SlashAnimInstance C++ class based in UAnimInstance to derive animation logic from c++
    - Adjusted jump animations for the charactor (Start and Land)
    - Created a control rig for the character to implement inverse kinematics for the character effectors using IK bones

---
+++ Source/Project_Lady_Slash/Private/Characters/SlashAnimInstance.cpp
@@ -0,0 +1,26 @@
+#include "Characters/SlashAnimInstance.h"
+#include "Characters/SlashCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+
+void USlashAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	EchoCharacter = Cast<ASlashCharacter>(TryGetPawnOwner());
+	if (EchoCharacter)
+	{
+		EchoMovementComponent = EchoCharacter->GetCharacterMovement();
+	}
+}
+
+void USlashAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+	if (EchoMovementComponent)
+	{
+		EchoGroundSpeed = UKismetMathLibrary::VSizeXY(EchoMovementComponent->Velocity);
+		IsEchoFalling = EchoMovementComponent->IsFalling();
+	}
+}
---
+++ Source/Project_Lady_Slash/Public/Characters/SlashAnimInstance.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SlashAnimInstance.generated.h"
+
+UCLASS()
+class PROJECT_LADY_SLASH_API USlashAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void NativeInitializeAnimation() override;
+
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation Variables")
+	class ASlashCharacter* EchoCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation Variables")
+	class UCharacterMovementComponent* EchoMovementComponent;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation Variables")
+	float EchoGroundSpeed;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation Variables")
+	bool IsEchoFalling;
+	
+};
